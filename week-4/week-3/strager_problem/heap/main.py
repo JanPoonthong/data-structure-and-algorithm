77aae657d0a006049dd9ffed505ac360344b0ab2
Jan Poonthong <janpoonthong628@gmail.com>
2023-07-20 14:27:50 +0700

    fix: heap

--- week-4/week-3/strager_problem/heap/main.py
+++ week-4/week-3/strager_problem/heap/main.py
@@ -1,128 +1,82 @@
-import math
 import time
 
-"""
-input: 16 14 10 8 7 9 3 2 4 1
-output:
-    16     2^0
-    14 10  2 ^ 1
-    8 7 9 3   2  ^ 2
-    2 4 1 _ _ _ _ _
 
-(2 ^ 2)  + (2 ^ 1) + (2 ^ 0) = n
+class Heap:
+    def __init__(self, items=None):
+        self.a = items[:] if items is not None else []
+        self.heap_size = len(self.a)
+        self.build_heap()
 
-(2 ^ 0) + (2 ^ 1) + (2 ^ 2) + (2 ^ 3) + (2 ^ 4) = 31
-(2 ^ 0) + (2 ^ 1) + (2 ^ 2) + (2 ^ 3) = 15
-(2 ^ 0) + (2 ^ 1) + (2 ^ 2) = 7
-(2 ^ 0) + (2 ^ 1) = 3
-(2 ^ 0)  = 1
+    @staticmethod
+    def compare(x, y):
+        return x < y
 
-total element up to the height = (2 ** height)  - 1
-                                 height = floor (log base 2 (total) + 1)
+    def empty(self):
+        return self.heap_size == 0
 
-log (total + 1)
- n = 1    n  = 2     n = 4    n = 8   n  = 16
-"""
-
-# 16 14 10 8 7 9 3 2 4 1
-
-
-"""
-1. building a heap by inserting numbers
-2. maximum or min; building a sorted list using the heap
-"""
-
-
-class MaxHeap:
-    arr = []
-
-    def __init__(self, max_size):
-        self.max_size = max_size
-        self.arr = [None] * max_size
-        self.heap_size = 0
-
-    def has_parent(self, a_list, last_index):
-        parent_index = last_index // 2
-        if parent_index > 0:
-            return [True, parent_index]
-        return [False, None]
-
-    def swap(self, a_list, pos1, pos2):
-        temp = a_list[pos1]
-        a_list[pos1] = a_list[pos2]
-        a_list[pos2] = temp
-
-    def has_child(self, a_list):
-        # algorithm left = 2*i and right = 2*i + 1
-
-        for index in range(len(a_list)):
-            has_left = 2 * index
-            has_right = (2 * index) + 1
-
-            if has_left > len(a_list) - 1:
-                a_list.append("_")
-            elif has_right > len(a_list) - 1:
-                a_list.append("_")
-
-    def debug(self):
-        # has_child(A)
-
-        i = 1
-        j = 0
-        while i < math.ceil(math.log(len(A) + 1, 2)) + 1:
-            while j < (2**i) - 1 and j < len(A):
-                print(A[j], end=" ")
-                j += 1
-            i += 1
-            print()
-
-    def build_heap(self, a_list, number):
-        self.insert(a_list, number)
-
-    def insert(self, a_list, number):
-        a_list.append(number)
-        last_index = len(a_list)
-
-        is_parent_exist = True
-        while is_parent_exist:
-            is_parent_exist, parent_index = self.has_parent(a_list, last_index)
-
-            if not is_parent_exist:
+    def heapify_up(self, i):
+        while i > 0:
+            parent = (i - 1) // 2
+            if self.compare(self.a[i], self.a[parent]):
+                self.a[i], self.a[parent] = self.a[parent], self.a[i]
+                i = parent
+            else:
                 break
 
-            is_parent_exist, up_up_parent_index = self.has_parent(a_list, parent_index)
+    def heapify_down(self, i):
+        while True:
+            smallest = i
+            left_child = 2 * i + 1
+            right_child = 2 * i + 2
 
-            if up_up_parent_index is not None:
-                left_child_of_up_up_parent_index = up_up_parent_index * 2
-                right_child_of_up_up_parent_index = (up_up_parent_index * 2) + 1
-            else:
-                root = parent_index
-                left_child_of_up_up_parent_index = root
-                right_child_of_up_up_parent_index = root
+            if left_child < self.heap_size and self.compare(self.a[left_child], self.a[smallest]):
+                smallest = left_child
 
-            if (
-                a_list[left_child_of_up_up_parent_index - 1]
-                < a_list[right_child_of_up_up_parent_index - 1]
-            ):
-                self.swap(a_list, last_index - 1, left_child_of_up_up_parent_index - 1)
-                last_index = left_child_of_up_up_parent_index
-            else:
-                self.swap(a_list, last_index - 1, right_child_of_up_up_parent_index - 1)
-                last_index = right_child_of_up_up_parent_index
+            if right_child < self.heap_size and self.compare(self.a[right_child], self.a[smallest]):
+                smallest = right_child
 
+            if smallest == i:
+                break
+
+            self.a[i], self.a[smallest] = self.a[smallest], self.a[i]
+            i = smallest
+
+    def insert(self, x):
+        self.heap_size += 1
+        if len(self.a) < self.heap_size:
+            self.a.append(x)
+        else:
+            self.a[self.heap_size - 1] = x
+        self.heapify_up(self.heap_size - 1)
+
+    def extract(self):
+        if self.empty():
+            raise ValueError("Error: Heap is empty")
+        x = self.a[0]
+        self.a[0] = self.a[self.heap_size - 1]
+        self.heap_size -= 1
+        self.heapify_down(0)
+        return x
+
+    def build_heap(self):
+        for i in range(self.heap_size // 2 - 1, -1, -1):
+            self.heapify_down(i)
+
+
+A = list(map(int, input().split()))
 
 st = time.process_time()
+h = Heap(items=A)
 
-b = list(map(int, input().split()))
-A = []
-heap = MaxHeap()
-
-for i in range(len(b)):
-    heap.build_heap(A, b[i])
+total = 0
+while h.heap_size > 1:
+    first = h.extract()
+    second = h.extract()
+    print(first, second)
+    temp = first + second
+    total += temp
+    h.insert(temp)
 
 et = time.process_time()
 
-heap.debug()
-print(A)
-
-print(f"Running time: {et - st}")
+print(total)
