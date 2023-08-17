db2f3602d1dc51711a69f306b722050404c68141
Jan Poonthong <janpoonthong628@gmail.com>
2023-08-17 10:44:22 +0700

    chore: add postfixEval0.py

---
+++ week-9/Week-8/postfixEval0.py
@@ -0,0 +1,43 @@
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+
+Stack = []
+stack_size = 0
+postfix_expr = input().split()
+for token in postfix_expr:
+    if is_number(token):
+        Stack.append(int(token))
+        stack_size += 1
+    else:
+        if token == "+":
+            Stack[stack_size - 2] = (
+                Stack[stack_size - 1] + Stack[stack_size - 2]
+            )
+            stack_size -= 1
+            Stack.pop()
+        elif token == "-":
+            Stack[stack_size - 2] = abs(
+                Stack[stack_size - 1] - Stack[stack_size - 2]
+            )
+            stack_size -= 1
+            Stack.pop()
+        elif token == "*":
+            Stack[stack_size - 2] = (
+                Stack[stack_size - 1] * Stack[stack_size - 2]
+            )
+            stack_size -= 1
+            Stack.pop()
+        elif token == "/":
+            Stack[stack_size - 2] = (
+                Stack[stack_size - 1] / Stack[stack_size - 2]
+            )
+            stack_size -= 1
+            Stack.pop()
+        print(Stack)
+
+print("%.1f" % Stack[0])
